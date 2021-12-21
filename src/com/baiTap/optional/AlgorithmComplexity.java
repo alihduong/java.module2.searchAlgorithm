8749531922f44ec65d26b36dbd532f25d93b888b
alihduong <nguyenhoangduong0711@gmail.com>
2021-12-21 09:17:36 +0700

    add file

--- src/com/baiTap/optional/AlgorithmComplexity.java
+++ src/com/baiTap/optional/AlgorithmComplexity.java
@@ -1,2 +1,24 @@
-package com.baiTap.optional;public class AlgorithmComplexity {
+package com.baiTap.optional;
+
+import java.util.Scanner;
+
+public class AlgorithmComplexity {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String inputString = scanner.nextLine();
+        int[] chars = new int[255];
+        for (int i = 0; i < inputString.length(); i++) {
+            int ascii = inputString.charAt(i);
+            chars[ascii]++;
+        }
+        int max = 0;
+        char character = (char) 255;
+        for (int j = 0; j < 255; j++)
+            if (chars[j] > max) {
+                max = chars[j];
+                character = (char) j;
+            }
+        System.out.println("The most appearing letter is '" + character + "' with a frequency of " + max + " times");
+    }
 }
